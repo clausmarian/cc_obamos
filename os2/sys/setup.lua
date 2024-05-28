541e868c183dc1e8bb5301df35f789dfbd87de99
4eclm <lp4el0l@protonmail.com>
2024-05-29 00:01:48 +0200

    added setup and updated startup

---
+++ os2/sys/setup.lua
@@ -0,0 +1,42 @@
+local function readNumber(text)
+  write(text)
+  return tonumber(read())
+end
+
+local function readVector3()
+  local x = readNumber("x: ")
+  if x == nil then
+    return nil
+  end
+
+  local y = readNumber("y: ")
+  if y == nil then
+    return nil
+  end
+
+  local z = readNumber("z: ")
+  if z == nil then
+    return nil
+  end
+
+  return vector.new(x, y, z)
+end
+
+-- gps
+print("\nIf you want this machine to act as GPS server, enter its position: ")
+local position = readVector3()
+if position == nil then
+  print("This machine will not act as a GPS server.")
+else
+  settings.set("os2.position", position)
+  settings.set("os2.gps_server", true)
+  print("This machine will act as GPS server.")
+end
+
+settings.set("motd.enable", false)
+settings.set("os2.setup", true)
+settings.save()
+
+print("\nSetup complete, press any key to reboot..")
+read()
+os.reboot()
--- startup/boot.lua
+++ startup/boot.lua
@@ -1,5 +1,70 @@
--- open all modems
-peripheral.find("modem", rednet.open)
+Vec2 = require("/os2/lib/lua/math/vec2")
+require("/os2/lib/lua/ui/animations")
 
--- setup path
-shell.setPath(shell.path() .. ":/os2/programs/reactor")
+local function clear()
+  term.setBackgroundColor(colors.black)
+  term.setTextColor(colors.white)
+  term.clear()
+
+  term.setCursorPos(1, 1)
+end
+
+SHOW_SPLASH = true
+
+local function splash()
+  clear()
+
+  local width, height = term.getSize()
+  term.setCursorPos(width / 2, height / 2 - 1)
+  term.write("Os2")
+
+  local drawCircle = loading_circle(Vec2.new(width / 2, height / 2 + 1), 1, colors.blue, colors.lightBlue)
+  while SHOW_SPLASH do
+    drawCircle()
+    sleep(0.1)
+  end
+
+  clear()
+end
+
+local function boot()
+  settings.define("os2.gps_server", {
+    description = "Position of the machine",
+    default = false,
+    type = "boolean"
+  })
+
+  settings.define("os2.position", {
+    description = "Position of the machine",
+    default = nil,
+    type = "table"
+  })
+  settings.load()
+
+  -- open all modems
+  peripheral.find("modem", rednet.open)
+
+  -- setup path
+  shell.setPath(shell.path() .. ":/os2/programs/reactor")
+
+  if not settings.get("os2.setup", false) then
+    SHOW_SPLASH = false
+    shell.run("/os2/sys/setup")
+  end
+
+  -- gps
+  local position = settings.get("os2.position")
+  if settings.get("os2.gps_server") and position then
+    SHOW_SPLASH = false
+    shell.run("gps", "host " .. tostring(position.x) .. " " .. tostring(position.y) .. " " .. tostring(position.z))
+  end
+
+  SHOW_SPLASH = false
+end
+
+parallel.waitForAll(splash, boot)
+
+-- autostart
+if fs.exists("/autostart.lua") then
+  require("/autostart")
+end
