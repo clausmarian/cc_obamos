05c06afe95e3b6c742f1854e12257d9dc132b840
4eclm <lp4el0l@protonmail.com>
2024-06-15 22:16:33 +0200

    added explorer to path

--- startup/boot.lua
+++ startup/boot.lua
@@ -45,7 +45,7 @@ local function boot()
   peripheral.find("modem", rednet.open)
 
   -- setup path
-  shell.setPath(shell.path() .. ":/os2/programs/reactor")
+  shell.setPath(shell.path() .. ":/os2/programs/reactor:/os2/programs/explorer")
 
   if not settings.get("os2.setup", false) then
     SHOW_SPLASH = false
