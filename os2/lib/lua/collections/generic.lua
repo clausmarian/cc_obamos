da7b8b5d6a997689cb0a56d86cb50ae688fbaa3a
4eclm <lp4el0l@protonmail.com>
2024-06-15 20:27:31 +0200

    added funcs

--- os2/lib/lua/collections/generic.lua
+++ os2/lib/lua/collections/generic.lua
@@ -21,3 +21,26 @@ function table:max()
 
   return max
 end
+
+function table:find(predicate)
+  for _, v in ipairs(self) do
+    if predicate(v) then
+      return v
+    end
+  end
+
+  return nil
+end
+
+function table:findRemove(findPredicate, removePredicate)
+  for i, v in ipairs(self) do
+    if removePredicate(v) then
+      table.remove(self, i)
+    elseif findPredicate(v) then
+      return v
+    end
+  end
+
+  return nil
+end
+
