0d89cc14f8d11ffbfe7463a6cff9f1ca954b7e29
4eclm <lp4el0l@protonmail.com>
2024-06-18 00:19:27 +0200

    fix client going into infinite loop when server never responds

--- os2/lib/lua/net/lttp/client.lua
+++ os2/lib/lua/net/lttp/client.lua
@@ -5,14 +5,16 @@ local Socket = import("net/socket")
 
 local Client = class("Client")
 
-function Client:new(protocol, serverPort, serverAddr, timeout)
+function Client:new(protocol, serverPort, serverAddr, timeout, tries)
   local o = {
     socket = Socket:client(),
     protocol = protocol,
     serverPort = serverPort,
     serverAddr = serverAddr,
-    timeout = timeout or 3
+    timeout = timeout or 3,
+    tries = tries or 5
   }
+  o.timeout = o.timeout / o.tries
 
   setmetatable(o, self)
   return o
@@ -26,9 +28,11 @@ function Client:get(endpoint, payload, payload_checker)
   self.socket:send(self.serverAddr, self.serverPort, Request.get(endpoint, payload), self.protocol)
 
   -- catch server response
+  local tries = 0
   local sender, packet, srcPort = nil, nil, nil
-  while sender ~= self.serverAddr or srcPort ~= self.serverPort or packet == nil or packet.endpoint ~= endpoint do
+  while tries < self.tries and (sender ~= self.serverAddr or srcPort ~= self.serverPort or packet == nil or packet.endpoint ~= endpoint) do
     sender, srcPort, packet, _ = self.socket:receive(self.protocol, self.timeout)
+    tries = tries + 1
   end
 
   local response = Response.parse(packet)
