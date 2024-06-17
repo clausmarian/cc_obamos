db2c70c5d31e84ce93008eb481d9f65c8d1c2c3d
4eclm <lp4el0l@protonmail.com>
2024-06-17 20:31:29 +0200

    extend struct

--- os2/lib/lua/parser/struct.lua
+++ os2/lib/lua/parser/struct.lua
@@ -2,12 +2,35 @@ require("/os2/lib/lua/std")
 
 local Struct = class("Struct")
 
-function Struct:new(template)
+function Struct:new(template, optional)
   local o = {
     template = template,
+    optional = optional or false
   }
   setmetatable(o, self)
   return o
+end
+
+local function check(template, t)
+  for key, vType in pairs(template) do
+    local value = t[key]
+
+    if isinstanceof(vType, Struct) then
+      if not (value == nil and vType.optional) and not check(vType.template, value) then
+        return false
+      end
+    else
+      if (type(vType) == "table" and type(value) ~= "table") or (type(vType) ~= "table" and type(value) ~= vType) then
+        return false
+      end
+
+      if type(vType) == "table" and not check(vType, value) then
+        return false
+      end
+    end
+  end
+
+  return true
 end
 
 function Struct:check(t)
@@ -16,14 +39,68 @@ function Struct:check(t)
   end
 
   if type(self.template) == "table" then
-    for key, vType in pairs(self.template) do
-      if type(t[key]) ~= vType then
-        return false
-      end
+    if not check(self.template, t) then
+      return false
     end
   end
 
   return true
+end
+
+local function fromFile(path)
+  if not fs.exists(path) then
+    return false
+  end
+
+  local file = fs.open(path, "r")
+  local t = textutils.unserialiseJSON(file.readAll())
+  file.close()
+
+  if t == nil then
+    return false
+  end
+
+  return true, t
+end
+
+local function toFile(path, t)
+  fs.makeDir(fs.getDir(path))
+  local file = fs.open(path, "w")
+  file.write(textutils.serialiseJSON(t))
+  file.close()
+  return true
+end
+
+function Struct:fromFile(path, list)
+  local suc, t = fromFile(path)
+
+  if not suc then
+    return false
+  end
+
+  if list then
+    if type(t) ~= "table" then
+      return false
+    end
+
+    for _, ti in ipairs(t) do
+      if not self:check(ti) then
+        return false
+      end
+    end
+  elseif not self:check(t) then
+    return false
+  end
+
+  return true, t
+end
+
+function Struct:toFile(path, t)
+  if not self:check(t) then
+    return false
+  end
+
+  return toFile(path, t)
 end
 
 return Struct
